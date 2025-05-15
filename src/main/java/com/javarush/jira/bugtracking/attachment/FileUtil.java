3adb2eb3e0a29d439f1ad392ae89c58753b50aaa
Nadezhda Shpak <ket180@yandex.ru>
2025-05-15 12:34:07 +0300

    Task 06 REFACTORING FileUtil#upload

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -7,14 +7,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @UtilityClass
 public class FileUtil {
@@ -24,16 +23,26 @@ public class FileUtil {
         if (multipartFile.isEmpty()) {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
-
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
-            }
+        Path dir = Paths.get(directoryPath).toAbsolutePath().normalize();
+        Path file = dir.resolve(fileName).normalize();
+        if (!file.startsWith(dir)) {
+            throw new IllegalRequestDataException("Invalid file name.");
         }
+        try {
+            if (!Files.exists(dir)) {
+                Files.createDirectories(dir);
+            }
+            try (InputStream inputStream = multipartFile.getInputStream()) {
+                Files.copy(
+                        inputStream,
+                        file,
+                        StandardCopyOption.REPLACE_EXISTING
+                );
+            }
+        } catch (IOException ex) {
+            throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
+        }
+
     }
 
     public static Resource download(String fileLink) {
